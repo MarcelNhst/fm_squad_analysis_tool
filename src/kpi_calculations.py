8e1ea46c4cc0cb8b19b5312be34484d159b9e542
Marcel <mnuhst@gmail.com>
2023-12-15 13:07:56 +0100

    added docstrings to functions

--- src/kpi_calculations.py
+++ src/kpi_calculations.py
@@ -1,6 +1,24 @@
 import logging
 
 def calculate_kpi_score(df, essential, core, secondary):
+    """
+    Calculate a Key Performance Indicator (KPI) score based on the provided DataFrame and attribute lists.
+
+    The KPI score is calculated by assigning different weights to essential, core, and secondary attributes
+    in the DataFrame. The weights are 5 for essential attributes, 3 for core attributes, and 1 for secondary attributes.
+
+    Parameters:
+    - df (pd.DataFrame): The DataFrame containing the data.
+    - essential (list): A list of essential attributes used for KPI calculation.
+    - core (list): A list of core attributes used for KPI calculation.
+    - secondary (list): A list of secondary attributes used for KPI calculation.
+
+    Returns:
+    - float: The calculated KPI score.
+
+    Raises:
+    - Exception: If the total number of fields for KPI calculation is zero.
+    """
     score = 0
     div = len(essential) * 5 + len(core) * 3 + len(secondary)
     if div == 0:
@@ -24,6 +42,26 @@ def calculate_kpi_score(df, essential, core, secondary):
 
 
 def calculate_custom_kpi_score(df, essential, core, secondary, tertiary):
+    """
+    Calculate a custom Key Performance Indicator (KPI) score based on the provided DataFrame and attribute lists.
+
+    The KPI score is calculated by assigning different weights to essential, core, secondary, and tertiary attributes
+    in the DataFrame. The weights are 5 for essential attributes, 3 for core attributes, 2 for secondary attributes,
+    and 1 for tertiary attributes.
+
+    Parameters:
+    - df (pd.DataFrame): The DataFrame containing the data.
+    - essential (list): A list of essential attributes used for KPI calculation.
+    - core (list): A list of core attributes used for KPI calculation.
+    - secondary (list): A list of secondary attributes used for KPI calculation.
+    - tertiary (list): A list of tertiary attributes used for KPI calculation.
+
+    Returns:
+    - float: The calculated custom KPI score.
+
+    Raises:
+    - Exception: If the total number of fields for KPI calculation is zero.
+    """
     score = 0
     div = len(essential) * 5 + len(core) * 3 + len(secondary) * 2 + len(tertiary)
     if div == 0:
